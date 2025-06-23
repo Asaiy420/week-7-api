965465881f0bbf580b7cd72d6f1280b587ab9bb2
Asaiy420 <kcaryan05@gmail.com>
2025-06-23 07:43:02 +0545

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import todoRoutes from "./routes/todo.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/todo", todoRoutes);
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
