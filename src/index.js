04e848703d243327d171a3edcc359aac20ab3e40
Asaiy420 <kcaryan05@gmail.com>
2025-06-24 07:01:19 +0545

    add: auth routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import todoRoutes from "./routes/todo.route.js";
+import authRoutes from "./routes/auth.route.js"; 
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./utils/db.js";
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/todo", todoRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
